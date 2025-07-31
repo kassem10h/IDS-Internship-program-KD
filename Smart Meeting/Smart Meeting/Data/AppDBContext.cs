778e150bb3c002de4158148e19c69428273ec645
Dana <danasakaan4@gmail.com>
2025-07-31 20:57:53 +0300

    fix conflicts

---
+++ Smart Meeting/Smart Meeting/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Smart_Meeting.Models;
+
+namespace SmartMeeting.Data
+{
+    public class AppDBContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDBContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Smart Meeting/Smart Meeting/Program.cs
+++ Smart Meeting/Smart Meeting/Program.cs
@@ -1,18 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Smart_Meeting.Models;
+using Smart_Meeting.Data;
 
 namespace Smart_Meeting
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+
+            builder.Services.AddDbContext<AppDBContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+
 
             var app = builder.Build();
 
@@ -23,12 +33,33 @@ namespace Smart_Meeting
                 app.UseSwaggerUI();
             }
 
+            // Middleware
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/error");
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
+
+            // Create Roles
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roles = { "Admin", "Employee", "Guest" };
+
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+            }
 
             app.Run();
         }
